61c4f2272b3ebc488ae705a34eb1087de9bb9d0d
“hatodog99” <paulpabahay@gmail.com>
2026-09-16 18:47:22 +0800

    lighter background color setup

--- main.cpp
+++ main.cpp
@@ -191,7 +191,8 @@ int main()
 
         processInput(window);
 
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        // background
+        glClearColor(0.085f, 0.085f, 0.085f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // bind Texture
