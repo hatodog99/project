fa1e0c7ccef464476225480eed9e1957ac0a9200
hatodog99 <paulpabahay@gmail.com>
2026-09-21 20:34:01 +0800

    fix fullscreen not toggling back to window

--- src/main.cpp
+++ src/main.cpp
@@ -146,21 +146,16 @@ void toggleFullscreen(GLFWwindow* window)
 {
     if (!isFullscreen)
     {
-        // remember current windowed position/size before switching
         glfwGetWindowPos(window, &windowedX, &windowedY);
         glfwGetWindowSize(window, &windowedWidth, &windowedHeight);
 
         GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-        int monitorX, monitorY;
-        glfwGetMonitorPos(monitor, &monitorX, &monitorY);
         const GLFWvidmode* mode = glfwGetVideoMode(monitor);
 
-        glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
-        glfwSetWindowMonitor(window, NULL, monitorX, monitorY, mode->width, mode->height, 0);
+        glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, 0);
     }
     else
     {
-        glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_TRUE);
         glfwSetWindowMonitor(window, NULL, windowedX, windowedY, windowedWidth, windowedHeight, 0);
     }
     isFullscreen = !isFullscreen;
